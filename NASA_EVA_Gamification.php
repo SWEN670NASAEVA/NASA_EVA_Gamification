9582e92ff8c43b5f46f8e68247a066a17da05b05
kevinfortier <kevin.fortier@gmail.com>
2018-02-23 20:04:27 -0500

    First try with extension.json

---
+++ NASA_EVA_Gamification.php
@@ -0,0 +1,11 @@
+<?php 
+
+
+$wgExtensionCredits['parserhook'][] = array(
+	'path' => __FILE__,
+	'name' => 'NASA_EVA_Gamification',
+	'url' => 'https://github.com/SWEN670NASAEVA/NASA_EVA_Gamification',
+	'version' => '0.1',
+	'author' => 'UMUC SWEN670 Spring 2018',
+#	'descriptionmsg' => 'youtube-desc',
+);
