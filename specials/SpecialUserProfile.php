1f24936f2237ecebf93fd8b4ec30c23c9cfc8eca
kevinfortier <kevin.fortier@gmail.com>
2018-03-14 13:11:23 -0400

    User profile working.  Profile and Game currently using hard-coded values

--- specials/SpecialUserProfile.php
+++ specials/SpecialUserProfile.php
@@ -18,36 +18,85 @@ class SpecialUserProfile extends SpecialPage {
 	 *  [[Special:UserProfile/subpage]].
 	 */
 	public function execute( $sub ) {
-//		global $wgOut;
 		$out = $this->getOutput();
-
-		$out->setPageTitle( $this->msg( 'gamification-userprofile' ) );
-
-//		$out->addHelpLink( 'How to become a MediaWiki hacker' );
 
 		$out->addWikiMsg( 'gamification-userprofile-intro' );
 
-//		$html = '<table class="wikitable"><tr><th>User ID</th><th>Badge Tag</th><th>Earned</th></tr>';
+		$out->addWikiText( 'Want to see [[Special:UserProfile/all|all]] badges earned?' );
 
-//		$dbr = wfGetDB( DB_SLAVE );
-//		$res = $dbr->select(
-//			'gamification_badges',
-//			array('user_id', 'badge_tag', 'date_badge_earned'),
-//			null,
-//			__METHOD__,
-//			array('ORDER BY' -> 'date_badge_earned ASC'),
-//			null
-//		);
-//		while ($row = $dbr->fetchRow($res)) {
-//			list($id, $tag, $earned) = array($row['user_id'], $row['badge_tag'], $row['date_badge_earned']);
-//			html .= "<tr><td>$id</td><td>$tag</td><td>$earned</td></tr>";
-//		}
-//		html .= "</table>";
-//
-//		$wgOut->addHTML( $html );
+		if( $sub == 'all') {
+			$this->userProfileAll();
+		} else {
+			$this->userProfileUser();
+		}
+
 	}
 
 	protected function getGroupName() {
 		return 'other';
+	}
+
+	public function userProfileUser() {
+		global $wgOut, $wgUser;
+		
+		$wgOut->setPageTitle(' User Gamification Profile for '.$wgUser->getName());
+
+		$dbr = wfGetDB( DB_SLAVE );
+		$res = $dbr->select(
+			'gamification_badges',
+			array('badge_tag', 'badge_rank', 'date_badge_earned'),
+			null,
+			__METHOD__,
+			array('ORDER BY' => 'date_badge_earned DESC'),
+			null
+		);
+		$txt = $wgUser->getName()."<br>";
+		$txt .= $wgUser->getRealName()."<br>";
+		$count = 0;
+		$text = "";
+		
+		while ($row = $dbr->fetchRow($res)) {
+			$text .= "The ".$row['badge_rank'].
+				" level ".$row['badge-tag']." badge was earned".
+				($row['date_badge_earned']==null ? "." : " on ".$row['date_badge_earned'].".<br>");
+			$count++;
+		}
+		$txt .= "You have earned ".$count." badge".($count == 1 ? "" : "s").".";
+		if($count > 0) {
+			$txt .= "  They are:<br>".$text;
+		}
+		
+		$wgOut->addWikiText( $txt );
+	}
+
+	public function userProfileAll() {
+		global $wgOut;
+
+		$wgOut->setPageTitle( 'User Gamification Profiles ' );
+
+		$html = '<table class="wikitable"><tr><th>User ID</th><th>Badge Tag</th><th>Badge Rank</th><th>Earned</th></tr>';
+
+		$dbr = wfGetDB( DB_SLAVE );
+		$res = $dbr->select(
+			array('gamification_badges','user'),
+			array('user.user_name',
+				'gamification_badges.badge_tag',
+				'gamification_badges.badge_rank', 
+				'gamification_badges.date_badge_earned'),
+			'user.user_id = gamification_badges.user_id',
+			__METHOD__,
+			array('ORDER BY' => 'date_badge_earned'),
+			null
+		);
+
+		while( $row = $dbr->fetchRow($res)) {
+			list($name, $tag, $rank, $earned) = 
+				array($row['user_name'], $row['badge_tag'], $row['badge_rank'], $row['date_badge_earned']);
+			$html .= "<tr><td>$name</td><td>$tag</td><td>$rank</td><td>$earned</td></tr>";
+		}		
+
+		$html .= "</table>";
+
+		$wgOut->addHTML( $html );
 	}
 }
