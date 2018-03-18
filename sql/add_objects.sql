2821a57b7e4e54d5fba0450720d75f069826dab9
kevinfortier <kevinfortier@users.noreply.github.com>
2018-03-18 14:03:04 -0400

    Update add_objects.sql
    SQLite compatibility (Try#3)

--- sql/add_objects.sql
+++ sql/add_objects.sql
@@ -4,11 +4,12 @@
 -- Table structure for table gamification_badges
 --
 
-CREATE TABLE IF NOT EXISTS gamification_badges (
-  user_id int(10) UNSIGNED NOT NULL PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS /*_*/ gamification_badges (
+  user_id int(10) UNSIGNED NOT NULL,
   badge_tag varchar(255) NOT NULL DEFAULT '',
   badge_rank varchar(255) NOT NULL DEFAULT '',
-  date_badge_earned binary(14) DEFAULT NULL
+  date_badge_earned binary(14) DEFAULT NULL,
+  PRIMARY KEY(user_id, badge_tag, badge_rank)
 ) ENGINE=InnoDB DEFAULT CHARSET=binary;
 
 --
@@ -24,16 +25,16 @@ CREATE TABLE IF NOT EXISTS gamification_badges (
 --  ADD KEY game_badge_search_idx (user_id,badge_tag,badge_rank) USING BTREE,
 --  ADD KEY gam_badge_user_idx (user_id) USING BTREE;
 
---CREATE INDEX IF NOT EXISTS game_badge_user_idx ON gamification_badges (user_id);
+CREATE UNIQUE INDEX /*i*/gamification_badges_unique_idx ON /*_*/ gamification_badges(user_id, badge_tag, badge_rank);
 
 --
 -- Constraints for table gamification_badges
 --
 
---ALTER TABLE gamification_badges
---  ADD CONSTRAINT gamification_badges_ibfk_1 FOREIGN KEY (user_id) REFERENCES `user` (user_id) 
---  ON DELETE CASCADE ON UPDATE CASCADE;
---COMMIT;
+ALTER TABLE /*_*/ gamification_badges 
+  ADD CONSTRAINT gamification_badges_ibfk_1 FOREIGN KEY (user_id) REFERENCES `user` (user_id) 
+  ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
 
 --
 -- Populate the gamification_badges table with the email verification badge, if they already verified before the extention was loaded.
