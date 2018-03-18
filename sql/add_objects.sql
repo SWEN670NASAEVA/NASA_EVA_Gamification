bfc4f30bbaec680a77b7a772c80fae4a9984b3a3
kevinfortier <kevinfortier@users.noreply.github.com>
2018-03-18 13:25:13 -0400

    Update add_objects.sql
    Changing script to get close to SQLite formatting

--- sql/add_objects.sql
+++ sql/add_objects.sql
@@ -5,10 +5,10 @@
 --
 
 CREATE TABLE IF NOT EXISTS gamification_badges (
-  user_id int(10) UNSIGNED NOT NULL,
-  badge_tag varbinary(255) NOT NULL DEFAULT '',
-  badge_rank varbinary(255) NOT NULL DEFAULT '',
-  date_badge_earned varbinary(14) DEFAULT NULL
+  user_id int(10) UNSIGNED NOT NULL PRIMARY KEY,
+  badge_tag varchar(255) NOT NULL DEFAULT '' PRIMARY KEY,
+  badge_rank varchar(255) NOT NULL DEFAULT '' PRIMARY KEY,
+  date_badge_earned binary(14) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=binary;
 
 --
@@ -19,24 +19,25 @@ CREATE TABLE IF NOT EXISTS gamification_badges (
 -- Indexes for table gamification_badges
 --
 
-ALTER TABLE gamification_badges
-  ADD PRIMARY KEY (user_id,badge_tag,badge_rank),
-  ADD KEY game_badge_search_idx (user_id,badge_tag,badge_rank) USING BTREE,
-  ADD KEY gam_badge_user_idx (user_id) USING BTREE;
+--ALTER TABLE gamification_badges
+--  ADD PRIMARY KEY (user_id,badge_tag,badge_rank),
+--  ADD KEY game_badge_search_idx (user_id,badge_tag,badge_rank) USING BTREE,
+--  ADD KEY gam_badge_user_idx (user_id) USING BTREE;
+
+CREATE INDEX IF NOT EXISTS game_badge_user_idx ON gamification_badges (user_id);
 
 --
 -- Constraints for table gamification_badges
 --
 
-ALTER TABLE gamification_badges
-  ADD CONSTRAINT gamification_badges_ibfk_1 FOREIGN KEY (user_id) REFERENCES `user` (user_id) 
-  ON DELETE CASCADE ON UPDATE CASCADE;
-COMMIT;
+--ALTER TABLE gamification_badges
+--  ADD CONSTRAINT gamification_badges_ibfk_1 FOREIGN KEY (user_id) REFERENCES `user` (user_id) 
+--  ON DELETE CASCADE ON UPDATE CASCADE;
+--COMMIT;
 
 --
 -- Populate the gamification_badges table with the email verification badge, if they already verified before the extention was loaded.
 --
-
 INSERT INTO gamification_badges (user_id, badge_tag, badge_rank)
   SELECT user_id, 'gamification-badge-emailverification', 'gamification-rank-1'
   FROM user
