c7e32cc0e077ce820cdf719873f2e325cd561c23
msalgoumuc <36683715+msalgoumuc@users.noreply.github.com>
2018-03-20 23:15:59 -0400

    First crack at User Gamification Profile
    I updated the User Gamification Profile to be more in line with our design. I tried images as well, but could not get that to work - placeholder is present and image files are present as well.

--- specials/SpecialUserGamificationProfile.php
+++ specials/SpecialUserGamificationProfile.php
@@ -20,7 +20,7 @@ class SpecialUserGamificationProfile extends SpecialPage {
 	public function execute( $sub ) {
 		$out = $this->getOutput();
 
-		$out->addWikiMsg( 'gamification-userprofile-intro' );
+		//$out->addWikiMsg( 'gamification-userprofile-intro' );
 
 		// Change the display, based on the subpage toggle
 		if( $sub == 'all') {
@@ -49,7 +49,7 @@ class SpecialUserGamificationProfile extends SpecialPage {
 			return true;
 		}
 
-		$wgOut->setPageTitle(' User Gamification Profile for '.$wgUser->getName());
+		$wgOut->setPageTitle(' User Gamification Profile');
 
 		// Query against a read-only database, if configured
 		$dbr = wfGetDB( DB_SLAVE );
@@ -63,62 +63,41 @@ class SpecialUserGamificationProfile extends SpecialPage {
 		);
 
 		// Display user's Name and RealName
-		$txt = $wgUser->getName()."<br>";
-		$txt .= $wgUser->getRealName()."<br>";
+		$html = "<b>" . "Username: " . "</b>" . $wgUser->getName() . "<br />";
+		$html .= "<b>" . "Name: " . "</b>" . ($wgUser->getRealName() == "" ? "[Not Populated]" : $wgUser->getRealName()) . "<br />";
 		$count = 0;
 		
 		// Get data from query while there are rows to be fetched
-		while ($row = $dbr->fetchRow($res)) {
-			$text .= "The ".wfMessage($row['badge_rank'])->escaped().
-				" level ".wfMessage($row['badge_tag'])->escaped()." badge was earned".
-				($row['date_badge_earned']==null ? "." : " on ".$row['date_badge_earned'].".<br>");
-			$count++;
-		}
-		$txt .= "You have earned ".$count." badge".($count == 1 ? "" : "s").".";
-		if($count > 0) {
-			$txt .= "  They are:<br>".$text;
+		for ($i = $wgNASA_EVA_GamificationMaxNumberOfRanks; $i > 0; $i--) {
+			${'rank' . $i . 'Count'} = 0;
+			${'rank' . $i . 'Array'} = array();
 		}
 		
-		$wgOut->addWikiText( $txt );
-
-		$wgOut->addWikiText( 'Want to see [[Special:UserGamificationProfile/all|all]] badges earned?' );
-	}
-
-	/**
-	  * Function will display all earned badges known to the system
-	  */
-
-	public function userGamificationProfileAll() {
-		global $wgOut;
-
-		$wgOut->setPageTitle( 'User Gamification Profiles ' );
-
-		$html = '<table class="wikitable"><tr><th>User Name</th><th>Badge Tag</th><th>Badge Rank</th><th>Earned</th></tr>';
-
-		$dbr = wfGetDB( DB_SLAVE );
-		$res = $dbr->select(
-			array('gamification_badges','user'),
-			array('user.user_name',
-				'gamification_badges.badge_tag',
-				'gamification_badges.badge_rank', 
-				'gamification_badges.date_badge_earned'),
-			'user.user_id = gamification_badges.user_id',
-			__METHOD__,
-			array('ORDER BY' => 'date_badge_earned'),
-			null
-		);
-
-		while( $row = $dbr->fetchRow($res)) {
-			list($name, $tag, $rank, $earned) = 
-				array($row['user_name'], 
-					wfMessage($row['badge_tag'])->escaped(), 
-					wfMessage($row['badge_rank'])->escaped(), 
-					($row['date_badge_earned']==null?"":wfTimestamp(TS_DB, $row['date_badge_earned'])));
-			$html .= "<tr><td>$name</td><td>$tag</td><td>$rank</td><td>$earned</td></tr>";
-		}		
-
-		$html .= "</table>";
-
+		$badgeRankArray = array();
+		while ($row = $dbr->fetchRow($res)) {
+			${'rank' . str_replace("gamification-rank-", "", $row['badge_rank']) . 'Count'}++;
+			${'rank' . str_replace("gamification-rank-", "", $row['badge_rank']) . 'Array'}[] = 
+				wfMessage($row['badge_tag'])->escaped() . " - " . ($row['date_badge_earned'] == null ? "." : date_format(date_create($row['date_badge_earned']), "m/d/Y") . "<br />");
+		}
+				
+		// Get and display badge data
+		$html .= '<table class="wikitable"><tr>';
+		for ($i = $wgNASA_EVA_GamificationMaxNumberOfRanks; $i > 0; $i--) {
+			$html .= '<th>' . '[' .wfMessage('gamification-rank-' . $i) . ' image]' . '</th>';
+		}
+		$html .= '</tr><tr>';
+		for ($i = $wgNASA_EVA_GamificationMaxNumberOfRanks; $i > 0; $i--) {
+			$html .= '<td>' . wfMessage('gamification-rank-' . $i) . " - " . ${'rank' . $i . 'Count'} . '</td>';
+		}
+		$html .= '</tr><tr>';
+		for ($i = $wgNASA_EVA_GamificationMaxNumberOfRanks; $i > 0; $i--) {
+			$html .= '<td>';
+			foreach (${'rank' . $i . 'Array'} as $rankarray) {
+				$html .= $rankarray;
+			}
+			$html .= '</td>';
+		}
+		$html .= "</tr></table>";
 		$wgOut->addWikiText( $html );
 	}
 }
